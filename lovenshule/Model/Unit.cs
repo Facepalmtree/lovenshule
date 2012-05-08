3f36753b2b87ef1c4a127ca51fb7f59bd664810f
simonfhansen@gmail.com <simonfhansen@gmail.com@e1447fa5-9966-c620-0890-b916df8805b9>
2012-05-08 08:27:04 +0000

    Har lavet attributes, properties og constructor for Unit

--- lovenshule/Model/Unit.cs
+++ lovenshule/Model/Unit.cs
@@ -6,7 +6,63 @@ using Interface;
 
 namespace Model
 {
-    abstract class Unit
+    public abstract class Unit
     {
+        int lives;
+        int point;
+        Hole currentHole;
+        int frame;
+        int maxFrame;
+        int damage;
+        int spawnTime;
+
+        public Unit(Hole currentHole)
+        {
+            this.currentHole = currentHole;
+        }
+
+        #region Methods
+
+        #endregion
+
+        #region Properties
+
+        public int Lives
+        {
+            get { return lives; }
+            set { lives = value; }
+        }
+        public int Point
+        {
+            get { return point; }
+            set { point = value; }
+        } 
+        internal Hole CurrentHole
+        {
+            get { return currentHole; }
+            set { currentHole = value; }
+        }
+        public int Frame
+        {
+            get { return frame; }
+            set { frame = value; }
+        }
+        public int MaxFrame
+        {
+            get { return maxFrame; }
+            set { maxFrame = value; }
+        }
+        public int Damage
+        {
+            get { return damage; }
+            set { damage = value; }
+        }
+        public int SpawnTime
+        {
+            get { return spawnTime; }
+            set { spawnTime = value; }
+        }
+        #endregion
+
     }
 }
