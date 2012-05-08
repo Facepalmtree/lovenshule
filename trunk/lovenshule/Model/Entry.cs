a6f51e3025e8c9917dff10a7d8ff38905903efac
Sander.Hansen@gmail.com <Sander.Hansen@gmail.com@e1447fa5-9966-c620-0890-b916df8805b9>
2012-05-08 08:07:58 +0000

    Entry variabler er programmeret.

--- trunk/lovenshule/Model/Entry.cs
+++ trunk/lovenshule/Model/Entry.cs
@@ -7,5 +7,12 @@ namespace Model
 {
     class Entry
     {
+        //Declaring class variables
+        private int score {get; set;}
+        //private Img Picture {get; set;}
+        private int time { get; set; }
+        private int levelCount { get; set; }
+        private int entryID { get; set; }
+        
     }
 }
