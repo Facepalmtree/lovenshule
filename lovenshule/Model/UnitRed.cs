058d45a29e3e1df3b26740803d574e52c98aa5d8
simonfhansen@gmail.com <simonfhansen@gmail.com@e1447fa5-9966-c620-0890-b916df8805b9>
2012-05-08 09:21:56 +0000

    Flere units, mangler korrekte værdier og metoder

---
+++ lovenshule/Model/UnitRed.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    class UnitRed : Unit
+    {
+        public UnitRed(Hole currentHole)
+            : base(currentHole)
+        {
+            //revise this!!!!!!!!!!!!!!!!
+            //this.Frame =
+            //this.MaxFrame =
+            this.Point = 1000;
+            this.Lives = 1;
+            this.Damage = 1;
+            this.SpawnTime = 1;
+        }
+    }
+}
