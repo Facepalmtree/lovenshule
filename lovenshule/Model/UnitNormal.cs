461bca6c530b22b089eda5f3b7e4cdc25a864fd3
simonfhansen@gmail.com <simonfhansen@gmail.com@e1447fa5-9966-c620-0890-b916df8805b9>
2012-05-08 08:58:34 +0000

    Har lavet UnitNormal som arver fra Unit. MANLGER: Definér frame, maxframe, point, spawntime

---
+++ lovenshule/Model/UnitNormal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    class UnitNormal : Unit
+    {
+        public UnitNormal(Hole currentHole)
+            : base(currentHole)
+        {
+            //revise this!!!!!!!!!!!!!!!!
+            //this.Frame =
+            //this.MaxFrame =
+            this.Point = 100;
+            this.Lives = 1;
+            this.Damage = 1;
+            this.SpawnTime = 1;
+        }
+    }
+}
