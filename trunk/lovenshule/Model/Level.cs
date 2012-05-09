317997710b6e648ca65c8fdfcc9dfa16e5b3c221
inzomnia.p2p@gmail.com <inzomnia.p2p@gmail.com@e1447fa5-9966-c620-0890-b916df8805b9>
2012-05-09 09:16:25 +0000

    Første udkast til funktionaliteten i Level.cs

--- trunk/lovenshule/Model/Level.cs
+++ trunk/lovenshule/Model/Level.cs
@@ -10,20 +10,67 @@ namespace Model
         public string background { get; set; }
         public decimal spawnFrequency { get; set; }
         public List<Hole> holes { get; set; }
+        public int holesCount { get; set; }
         public int startTime { get; set; }
         public int spawn { get; set; }
         public int spawnCount { get; set; }
         public int theme { get; set; }
 
-        public Level(string background, decimal spawnFrequency, List<Hole> holes, int startTime, int spawn, int spawnCount, int theme)
+        public Level()
         {
-            this.background = background;
-            this.spawnFrequency = spawnFrequency;
-            this.holes = holes;
-            this.startTime = startTime;
-            this.spawn = spawn;
-            this.spawnCount = spawnCount;
-            this.theme = theme;
+            startTime = holesCount = spawn = 0;
+            holes = new List<Hole>();
+
+        }
+
+        public void nextLevel(int level)
+        {           
+            switch (level)
+            {
+                case 1: 
+                    holesCount = 3;
+                    spawn = 10;
+
+                    break;
+                case 2:
+                    holesCount = 3;
+                    spawn = 20;
+                    break;
+                case 3: 
+                    holesCount = 5;
+                    spawn = 30;
+                    break;
+                case 4:
+                    holesCount = 5;
+                    spawn = 40;
+                    break;
+                case 5:
+                    holesCount = 7;
+                    spawn = 50;
+                    break;
+                case 6:
+                    holesCount = 7;
+                    spawn = 60;
+                    break;
+                case 7:
+                    holesCount = 9;
+                    spawn = 70;
+                    break;
+                case 8:
+                    holesCount = 9;
+                    spawn = 80;
+                    break;
+                case 9:
+                    holesCount = 9;
+                    spawn = 90;
+                    break;
+                case 10:
+                    holesCount = 9;
+                    spawn = 100;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
