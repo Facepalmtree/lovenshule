1891b193712f6ae64ad3848dc3b8d74ccdc29d1a
inzomnia.p2p@gmail.com <inzomnia.p2p@gmail.com@e1447fa5-9966-c620-0890-b916df8805b9>
2012-05-09 07:54:45 +0000

    Attributes tilføjet til PlayerData.cs

--- lovenshule/Model/PlayerData.cs
+++ lovenshule/Model/PlayerData.cs
@@ -7,5 +7,25 @@ namespace Model
 {
     class PlayerData
     {
+        public int score { get; set; }
+        public string picture { get; set; }
+        public int points { get; set; }
+        public Hole currentHole { get; set; }
+        public int frame { get; set; }
+        public int maxFrame { get; set; }
+        public int damage { get; set; }
+        public int spawnTime { get; set; }
+
+        public PlayerData( int score, string picture, int points, Hole currentHole, int frame, int maxFrame, int damage, int spawnTime )
+        {
+            this.score = score;
+            this.picture = picture;
+            this.points = points;
+            this.currentHole = currentHole;
+            this.frame = frame;
+            this.maxFrame = maxFrame;
+            this.damage = damage;
+            this.spawnTime = spawnTime;
+        }
     }
 }
