91b95de67a99e7cc8849225fb0b67ab01d1529b3
inzomnia.p2p@gmail.com <inzomnia.p2p@gmail.com@e1447fa5-9966-c620-0890-b916df8805b9>
2012-05-09 07:48:04 +0000

    Attributes til Level.cs tilføjet

--- trunk/lovenshule/Model/Level.cs
+++ trunk/lovenshule/Model/Level.cs
@@ -7,6 +7,23 @@ namespace Model
 {
     class Level
     {
-        j
+        public string background { get; set; }
+        public decimal spawnFrequency { get; set; }
+        public List<Hole> holes { get; set; }
+        public int startTime { get; set; }
+        public int spawn { get; set; }
+        public int spawnCount { get; set; }
+        public int theme { get; set; }
+
+        public Level(string background, decimal spawnFrequency, List<Hole> holes, int startTime, int spawn, int spawnCount, int theme)
+        {
+            this.background = background;
+            this.spawnFrequency = spawnFrequency;
+            this.holes = holes;
+            this.startTime = startTime;
+            this.spawn = spawn;
+            this.spawnCount = spawnCount;
+            this.theme = theme;
+        }
     }
 }
