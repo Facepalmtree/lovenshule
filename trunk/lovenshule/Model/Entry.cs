70ca9604fda88b74d33a4d6bbf0773d16bf99a48
simonfhansen@gmail.com <simonfhansen@gmail.com@e1447fa5-9966-c620-0890-b916df8805b9>
2012-05-14 10:05:33 +0000

    rettet constructor

--- trunk/lovenshule/Model/Entry.cs
+++ trunk/lovenshule/Model/Entry.cs
@@ -10,16 +10,16 @@ namespace Model
         //Declaring class variables
         public int score { get; set; }
         //private Img Picture {get; set;}
-        public int time { get; set; }
+        public int playTime { get; set; }
         public int levelCount { get; set; }
         public int entryID { get; set; }
 
-        public Entry(int scor, int tim, int levelC, int entry)
+        public Entry(int score, int playTime, int levelCount, int entryID)
         {
-            score = scor;
-            time = tim;
-            levelCount = levelC;
-            entryID = entry;
+            this.score = score;
+            this.playTime = playTime;
+            this.levelCount = levelCount;
+            this.entryID = entryID;
         }
     }
 }
