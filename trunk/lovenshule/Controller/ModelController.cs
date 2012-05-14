b70b1e826c8ec7dda1c0daff4c47d98bc15a3621
simonfhansen@gmail.com <simonfhansen@gmail.com@e1447fa5-9966-c620-0890-b916df8805b9>
2012-05-14 11:30:17 +0000

    'hent' og 'gem' stored procedures og tilhørende kald i dbcontroller og modelcontroller

--- trunk/lovenshule/Controller/ModelController.cs
+++ trunk/lovenshule/Controller/ModelController.cs
@@ -11,15 +11,67 @@ namespace Controller
     public class ModelController
     {
         //sessionvariables
+        DBController dbcontroller;
         PlayerData currentPlayer = null;
-        Highscore highscore = null;
+        Highscore highscore = new Highscore();
 
+        //TEST PLAYER
+        public void TestPlayer()
+        {
+            currentPlayer = new PlayerData(100, "trololol", 20, 5);
+        }
+
+
+
+        //constructor
+        public ModelController()
+        {
+            dbcontroller = new DBController(this);
+        }
 
         //methods
         public void HitMole(/*id på hul*/)
         {
 
         }
+
+        public void AddEntry()
+        {
+            //TEST
+            TestPlayer();
+
+            try
+            {
+                DateTime now = DateTime.Now;
+                int entryID = dbcontroller.AddEntry(currentPlayer.score, currentPlayer.time, currentPlayer.levelCount, now);
+
+                if (entryID != -1)
+                {
+                    highscore.AddEntry(new Entry(currentPlayer.score, currentPlayer.time, currentPlayer.levelCount, entryID, now)); 
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+#region Methods for DBController
+
+        //add entry to model from db
+        public void DB_AddEntry(int score, int playTime, int levelCount, int entryID, DateTime entryTime)
+        {
+            try
+            {
+                highscore.AddEntry(new Entry(score, playTime, levelCount, entryID, entryTime));
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+        }
+
+#endregion
 
 
         public void LoadHighscore()
