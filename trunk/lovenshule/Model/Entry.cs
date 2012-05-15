56f3a4a2dd2699c07423568dee048b7d6472bbfc
simonfhansen@gmail.com <simonfhansen@gmail.com@e1447fa5-9966-c620-0890-b916df8805b9>
2012-05-15 10:51:22 +0000

    tilføjet Image variabel til entry, og tilføjet system.drawing reference til model-projektet

--- trunk/lovenshule/Model/Entry.cs
+++ trunk/lovenshule/Model/Entry.cs
@@ -3,25 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using System.Drawing;
+
 namespace Model
 {
     public class Entry
     {
         //Declaring class variables
-        public int score { get; set; }
-        //private Img Picture {get; set;}
+        public int score { get; set; }        
         public int playTime { get; set; }
         public int levelCount { get; set; }
         public int entryID { get; set; }
         public DateTime entryTime { get; set; }
+        public Image image { get; set; }
 
-        public Entry(int score, int playTime, int levelCount, int entryID, DateTime entryTime)
+        public Entry(int score, int playTime, int levelCount, int entryID, DateTime entryTime, Image image)
         {
             this.score = score;
             this.playTime = playTime;
             this.levelCount = levelCount;
             this.entryID = entryID;
             this.entryTime = entryTime;
+            this.image = image;
         }
     }
 }
