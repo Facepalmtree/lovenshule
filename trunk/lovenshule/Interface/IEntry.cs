4d37e888f2e2f1bfa0c3eeee490bcc6fc4c4ab7a
simonfhansen@gmail.com <simonfhansen@gmail.com@e1447fa5-9966-c620-0890-b916df8805b9>
2012-05-16 07:01:39 +0000

    tilføjet interface til entry

---
+++ trunk/lovenshule/Interface/IEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+namespace Interface
+{
+    public interface IEntry
+    {
+        int entryID { get; set; }
+        DateTime entryTime { get; set; }
+        System.Drawing.Image image { get; set; }
+        int levelCount { get; set; }
+        int playTime { get; set; }
+        int score { get; set; }
+    }
+}
