93d1fa1d9611d42aea45d3e3066ce7ee09c8e902
Sander.Hansen@gmail.com <Sander.Hansen@gmail.com@e1447fa5-9966-c620-0890-b916df8805b9>
2012-05-18 08:52:25 +0000

    Committing IPlayerData

---
+++ lovenshule/Interface/IPlayerData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Interface
+{
+    public interface IPlayerData
+    {
+        void AddScore(int score);
+        int health { get; set; }
+        System.Drawing.Image image { get; set; }
+        int levelCount { get; set; }
+        void LoseLife(int hit);
+        void SaveTime(int time);
+        void SetImage(System.Drawing.Image image);
+        void SetLevel(int level);
+        int time { get; set; }
+        int totalScore { get; set; }
+        string UpdateHealth();
+        string UpdateScore();
+    }
+}
