b9245cf2a5505a0d596e29e206549f9f86e4864a
Sander.Hansen@gmail.com <Sander.Hansen@gmail.com@e1447fa5-9966-c620-0890-b916df8805b9>
2012-05-18 07:47:29 +0000

    Iplayer data was created

---
+++ lovenshule/Interface/IPlayerData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Model
+{
+    public interface IPlayerData
+    {
+        void AddScore(int score);
+        void LoseLife(int hit);
+        void SaveTime(int time);
+        void SetImage(System.Drawing.Image image);
+        void SetLevel(int level);
+        string UpdateHealth();
+        string UpdateScore();
+    }
+}
