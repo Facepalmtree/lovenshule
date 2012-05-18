ee19de0b0e11ff9b2e02902bebd370f294cea14f
Sander.Hansen@gmail.com <Sander.Hansen@gmail.com@e1447fa5-9966-c620-0890-b916df8805b9>
2012-05-18 08:32:36 +0000

    A basic game is there.. working!

--- lovenshule/Model/IPlayerData.cs
+++ lovenshule/Model/IPlayerData.cs
@@ -1,13 +1,18 @@
 ﻿using System;
-namespace Interface
+namespace Model
 {
-    public interface IPlayerData
+    interface IPlayerData
     {
         void AddScore(int score);
+        int health { get; set; }
+        System.Drawing.Image image { get; set; }
+        int levelCount { get; set; }
         void LoseLife(int hit);
         void SaveTime(int time);
         void SetImage(System.Drawing.Image image);
         void SetLevel(int level);
+        int time { get; set; }
+        int totalScore { get; set; }
         string UpdateHealth();
         string UpdateScore();
     }
