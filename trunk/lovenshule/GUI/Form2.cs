6e0d29a227d731b0dea9d6aab30c63967ad99c21
simonfhansen@gmail.com <simonfhansen@gmail.com@e1447fa5-9966-c620-0890-b916df8805b9>
2012-05-22 10:22:08 +0000

    rettet fejl + design

--- trunk/lovenshule/GUI/Form2.cs
+++ trunk/lovenshule/GUI/Form2.cs
@@ -21,13 +21,15 @@ namespace GUI
             InitializeComponent();
 
             controller = new ModelController();
+
+            this.BackgroundImage = Properties.Resources.background;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             Form webcam = new FrmWebcam(controller);
             webcam.ShowDialog();
-            controller.NewPlayer((Image)Properties.Resources.background);
+            
             if (controller.GetCurrentPlayer() != null)
             {
                 Form game = new Form1(controller);
@@ -47,9 +49,6 @@ namespace GUI
             controller.LoadHighscore();
 
             Label[] labels_score1 = { lblAscore1, lblAscore2, lblAscore3, lblAscore4, lblAscore5, lblAscore6, lblAscore7, lblAscore8, lblAscore9, lblAscore10 };
-            Label[] labels_tid1 = { lblAtid1, lblAtid2, lblAtid3, lblAtid4, lblAtid5, lblAtid6, lblAtid7, lblAtid8, lblAtid9, lblAtid10 };
-            Label[] labels_level1 = { lblAlevel1, lblAlevel2, lblALevel3, lblAlevel4, lblAlevel5, lblAlevel6, lblAlevel7, lblAlevel8, lblAlevel9, lblAlevel10 };
-            Label[] labels_dato1 = { lblAdato1, lblAdato2, lblAdato3, lblAdato4, lblAdato5, lblAdato6, lblAdato7, lblAdato8, lblAdato9, lblAdato10 };
             PictureBox[] pictureboxes1 = { pbxA1, pbxA2, pbxA3, pbxA4, pbxA5, pbxA6, pbxA7, pbxA8, pbxA9, pbxA10 };            
 
             List<IEntry> ihighscores = controller.GetIEntries();
@@ -65,9 +64,7 @@ namespace GUI
                         if (ientry.entryTime.Date == DateTime.Now.Date)
                         {
                             labels_score1[i].Text = ientry.score.ToString();
-                            labels_tid1[i].Text = ientry.playTime.ToString();
-                            labels_level1[i].Text = ientry.levelCount.ToString();
-                            labels_dato1[i].Text = ientry.entryTime.ToShortDateString();
+                            labels_score1[i].TextAlign = System.Drawing.ContentAlignment.MiddleRight;
                             pictureboxes1[i].Image = ientry.image;                           
                         }
                     }
@@ -78,26 +75,20 @@ namespace GUI
                     i++;
                 }
             }
-            else
+            
+            
+            while (i < 10)
             {
-                while (i < 10)
-                {
-                    labels_score1[i].Text = "---";
-                    labels_tid1[i].Text = "---";
-                    labels_level1[i].Text = "---";
-                    labels_dato1[i].Text = "---";
-                    pictureboxes1[i].Image = null;                    
-                    i++;
-                }
-
+                labels_score1[i].Text = "---";
+                pictureboxes1[i].Image = null;                    
+                i++;
             }
+
+            
 
             //for highscore list B, containing alltime highscores
 
             Label[] labels_score2 = { lblBscore1, lblBscore2, lblBscore3, lblBscore4, lblBscore5, lblBscore6, lblBscore7, lblBscore8, lblBscore9, lblBscore10 };
-            Label[] labels_tid2 = { lblBtid1, lblBtid2, lblBtid3, lblBtid4, lblBtid5, lblBtid6, lblBtid7, lblBtid8, lblBtid9, lblBtid10 };
-            Label[] labels_level2 = { lblBlevel1, lblBlevel2, lblBlevel3, lblBlevel4, lblBlevel5, lblBlevel6, lblBlevel7, lblBlevel8, lblBlevel9, lblBlevel10 };
-            Label[] labels_dato2 = { lblBdato1, lblBdato2, lblBdato3, lblBdato4, lblBdato5, lblBdato6, lblBdato7, lblBdato8, lblBdato9, lblBdato10 };
             PictureBox[] pictureboxes2 = { pbxB1, pbxB2, pbxB3, pbxB4, pbxB5, pbxB6, pbxB7, pbxB8, pbxB9, pbxB10 };            
 
             i = 0;
@@ -108,31 +99,25 @@ namespace GUI
                 {
                     if (i < 10)
                     {
-                        labels_score1[i].Text = ientry.score.ToString();
-                        labels_tid1[i].Text = ientry.playTime.ToString();
-                        labels_level1[i].Text = ientry.levelCount.ToString();
-                        labels_dato1[i].Text = ientry.entryTime.ToShortDateString();
-                        pictureboxes1[i].Image = ientry.image;                        
+                        labels_score2[i].Text = ientry.score.ToString();
+                        pictureboxes2[i].Image = ientry.image;                        
                     }
                     else
                     {
-                        break;
+                        break; 
                     }
                     i++;
                 }
             }
-            else
+            
+            
+            while (i < 10)
             {
-                while (i < 10)
-                {
-                    labels_score2[i].Text = "---";
-                    labels_tid2[i].Text = "---";
-                    labels_level2[i].Text = "---";
-                    labels_dato2[i].Text = "---";
-                    pictureboxes2[i].Image = null;                    
-                    i++;
-                }
+                labels_score2[i].Text = "---";                    
+                pictureboxes2[i].Image = null;                    
+                i++;
             }
+            
         }
 
         private void Form2_Load(object sender, EventArgs e)
